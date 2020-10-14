ec8868adf56d6831799475bb400529f2f1334d9c
rohitrajmishra <rohitrajmishra.nits@gmail.com>
2020-10-15 00:42:28 +0530

    List all posts on home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,10 @@
-module.exports.home = function(req, res) {
-  // return res.end('<h1> Express is up for Codeial</h1>')
-  console.log(req.cookies);
-  res.cookie('user_id', 25);
+const Post = require('../models/post');
 
-  return res.render('home', {
-    title: "MyHome"
-  });
+module.exports.home = function(req, res) {
+  Post.find({}).populate('user').exec(function (err, posts){
+    return res.render('home', {
+      title: "Codeial | MyHome",
+      posts: posts
+    });
+  })
 }
