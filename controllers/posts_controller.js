8482d53cf4610680c73bd33b50d44d11b32dfb85
rohitrajmishra <rohitrajmishra.nits@gmail.com>
2020-10-14 16:59:07 +0530

    Added post logic

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,3 +1,19 @@
+const Post = require('../models/post');
+
 module.exports.listAll = function(req, res) {
   return res.end('<h1> All posts</h1>')
+}
+
+module.exports.create = function (req, res){
+  Post.create({
+    content: req.body.content,
+    user: req.user._id
+  }, function (err, post){
+    if(err){
+      console.log('Error in creating the post');
+      return;
+    }
+
+    return res.redirect('back');
+  })
 }
--- routes/posts.js
+++ routes/posts.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const postsController = require('../controllers/posts_controller');
 
 router.get('/', postsController.listAll);
+router.post('/create', postsController.create);
 
 module.exports = router;
