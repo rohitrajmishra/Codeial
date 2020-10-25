70b81f716afca49f2043df2547b4b4aa00ebdc29
rohitrajmishra <rohitrajmishra.nits@gmail.com>
2020-10-25 09:03:58 +0530

    Added comments on post logic

--- models/comment.js
+++ models/comment.js
@@ -1,23 +1,23 @@
 const mongoose = require('mongoose');
 
-const postSchema = new mongoose.Schema({
+const commentSchema = new mongoose.Schema({
   content: {
     type: String,
     required: true
   },
+  // comment belongs to a user
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
   },
-  // include the array of ids of all comments in this post schema  itself
-  comments: [{
+  post: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Comment'
-  }]
+    ref: 'Post'
+  }
 }, {
   timestamps: true
 });
 
-const Post = mongoose.model('Post', postSchema);
+const Comment = mongoose.model('Comment', commentSchema);
 
-module.exports = Post;
+module.exports = Comment;
