2b5b5748eb9cfe1142dd0b7cd1660d2da3ce8c8b
rohitrajmishra <rohitrajmishra.nits@gmail.com>
2020-10-26 12:02:06 +0530

    Added - Edit/Update user details feature

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -11,6 +11,16 @@ module.exports.profile = function(req, res) {
 
 }
 
+module.exports.update = function(req, res) {
+  // Check if edit request came from logged in user
+  if(req.user.id == req.params.id){
+    User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
+      return res.redirect('back');
+    });
+  }else{
+    return res.status(401).send('Unauthorized');
+  }
+}
 module.exports.signin = function(req, res) {
   if(req.isAuthenticated()){
     return res.redirect('/users/profile');
