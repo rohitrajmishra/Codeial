d5300f139f846380ca0365d6256462a433466941
rohitrajmishra <rohitrajmishra.nits@gmail.com>
2020-10-29 11:37:42 +0530

    Added notifications for post publish and delete

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -5,17 +5,20 @@ const User = require('../models/user');
 
 // authentication using passport
 passport.use(new LocalStrategy({
-  usernameField: 'email'
+  usernameField: 'email',
+  passReqToCallback: true
   },
-  function(email, password, done){
+  function(req, email, password, done){
     // find the user and establish the identity
     User.findOne({email: email}, function (err, user){
       if(err){
+        req.flash('error', err);
         console.log('Error in finding user --> Passport');
         return done(err);
       }
 
       if(!user || user.password != password){
+        req.flash('error', 'Invalid Username/Password');
         console.log('Invalid Username/Password');
         return done(null, false);
       }
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -12,10 +12,13 @@ module.exports.create = async function(req, res) {
       user: req.user._id
     });
 
+    req.flash('success', 'Post published!');
     return res.redirect('back');
 
   } catch (err) {
+    req.flash('error', err);
     console.log('Error', err);
+    return res.redirect('back');
   }
 
 }
@@ -33,11 +36,15 @@ module.exports.destroy = async function(req, res) {
       await Comment.deleteMany({
         post: req.params.id
       });
+
+      req.flash('success', 'Post and associated comments deleted!');
       return res.redirect('back');
     } else {
+      req.flash('error', 'You can not delete this post!');
       return res.redirect('back');
     }
   } catch (err) {
+    req.flash('error', err);
     console.log('Error', err);
   }
 
