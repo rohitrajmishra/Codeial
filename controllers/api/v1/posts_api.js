0ea0a0ecae9c5e89773d447d5acc4001b65d0ee6
rohitrajmishra <rohitrajmishra.nits@gmail.com>
2020-11-12 08:09:38 +0530

    Add api directory structure and setup passport jwt

---
+++ controllers/api/v1/posts_api.js
@@ -0,0 +1,58 @@
+const Post = require('../../../models/post');
+const User = require('../../../models/user');
+const Comment = require('../../../models/comment');
+
+module.exports.index = async function (req, res) {
+
+  let posts = await Post.find({})
+    .sort('-createdAt')
+    .populate({
+      path: 'user',
+      select: '-password'
+    })
+    .populate({
+      path: 'comments',
+      populate: {
+        path: 'user'
+      }
+    });
+
+  return res.json(200, {
+    message: "List of posts",
+    posts: posts,
+  });
+};
+
+module.exports.destroy = async function(req, res) {
+
+  try {
+    let post = await Post.findById(req.params.id);
+
+    // id is automatic conversion to string from _id
+    // if (post.user == req.user.id) {
+      post.remove();
+
+      await Comment.deleteMany({
+        post: req.params.id
+      });
+
+      // req.flash('success', 'Post and associated comments deleted!');
+      // return res.redirect('back');
+
+      return res.json(200, {
+        message: "Post and associated comments deleted successfully!"
+      });
+    // }
+    // else {
+    //   req.flash('error', 'You can not delete this post!');
+    //   return res.redirect('back');
+    // }
+  } catch (err) {
+    // req.flash('error', err);
+    console.log('***********Error', err);
+    return res.json(500, {
+      message: "Internal server error"
+    });
+  }
+
+}
--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const User = require('./models/user')
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+const passportJwt = require('./config/passport-jwt-strategy');
+
 const MongoStore = require('connect-mongo')(session);
 const sassMiddleware = require('node-sass-middleware');
 const flash = require('connect-flash');
